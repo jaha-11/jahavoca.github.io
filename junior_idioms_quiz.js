535056122874e14c14eede3786ff0328b7f8cf06
jaha-11 <aqwiny1216@gmail.com>
2025-05-31 22:49:46 +0900

    Update junior_idioms_quiz.js

--- junior_idioms_quiz.js
+++ junior_idioms_quiz.js
@@ -14,8 +14,19 @@ const pauseButton = document.getElementById("pause-button");
 function speak(text) {
   try {
     const utter = new SpeechSynthesisUtterance(text);
-    utter.lang = 'en-US';
-    window.speechSynthesis.speak(utter);
+    const voices = window.speechSynthesis.getVoices();
+      
+      // 미국 원어민 목소리 중 하나 선택 (예: 'Samantha' or 'Daniel')
+      utter.voice = voices.find(voice => voice.lang === 'en-US' && voice.name.includes('Samantha'));
+      
+      // 대체 설정
+      utter.lang = 'en-US';
+      utter.rate = 1;
+      utter.pitch = 1;
+      speechSynthesis.speak(utter);
+
+
+
   } catch (e) {
     console.warn("발음 실패:", e);
   }
